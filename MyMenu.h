3c8e83b54aa3ec98b652b0305c5c4ee2f37041b1
Yuri Pachkovsky 550502 <32078512+YuriPachkovsky@users.noreply.github.com>
2018-07-16 10:57:31 +0300

    navigate and draw  main and sub menu

---
+++ MyMenu.h
@@ -0,0 +1,58 @@
+/* ===============================================
+|	Помогите мне я заебался 
+| ===============================================
+*/
+
+#ifndef MYMENU_H
+#define MYMENU_H
+
+#include <U8g2lib.h>
+
+class MItm
+{
+  public:
+    MItm(String _name, int _first, int _second = -1);
+	void addPars(int _sub_id, int _back_id, String _sub_title);
+    String name;
+    int first;
+    int second;
+	int sub_id;
+	int back_id;
+	String sub_title;
+};
+
+class Menu
+{
+  public:
+    Menu(MItm *_items, int _num_items, U8G2_ST7920_128X64_F_SW_SPI* _u8g2, void (*_callback)(int), int _num_rows = 4, String _cursor = ">");
+	void goNext();
+	void goBack();
+  	void goMain();
+	void goDown();
+	void goUp();
+	void goLast();
+	void goSub(int sub_idx);
+	void goItem(int item_idx);
+  private:
+	void drawSub();
+	int findFirst(MItm citm);
+	void (*callback)(int);
+
+    MItm *items;
+    U8G2_ST7920_128X64_F_SW_SPI *u8g2;  
+	int num_rows;
+	int cur_sub;
+	int cur_item;
+	int cur_sub_index;
+	
+	int subsize;
+	int fullsize;
+	
+	int cur_pos;
+	String cursor;
+	
+	String title;
+	String main_title;
+};
+
+#endif
